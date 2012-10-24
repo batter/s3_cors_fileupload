f37d237b3c4770daba4ed5d1b75c3afe124c32cf
Ben Atkins <benatkins@fullbridge.com>
2012-10-24 13:53:34 -0400

    Re-formatting the syntax of the :setup_routes method on the install generator so that it indicates what the routes will look like in the rails routes file.

--- lib/generators/s3_cors_fileupload/install/install_generator.rb
+++ lib/generators/s3_cors_fileupload/install/install_generator.rb
@@ -17,30 +17,31 @@ module S3CorsFileupload
       def create_migration_file
         migration_template 'create_source_files.rb', 'db/migrate/create_source_files.rb'
       end
-    
+
       def create_model_file
         copy_file 'source_file.rb', 'app/models/source_file.rb'
       end
-    
+
       def create_controller
         # if options.with_controller?
         copy_file 's3_uploads_controller.rb', 'app/controllers/s3_uploads_controller.rb'
         copy_file 's3_uploads.js', 'app/assets/javascripts/s3_uploads.js'
       end
-    
+
       def create_views
         Dir.foreach(File.expand_path('../templates/views', __FILE__)).reject { |file_name| %w(. ..).include?(file_name) }.each do |file_name|
           copy_file "views/#{file_name}", "app/views/s3_uploads/#{file_name}"
         end
       end
-    
+
       def setup_routes
         route(
           ["resources :source_files, :only => [:index, :create, :destroy], :controller => 's3_uploads' do", "\n",
-          "    get :generate_key, :on => :collection", "\n", "  end"].join
+        "    get :generate_key, :on => :collection", "\n",
+        "  end"].join
         )
       end
-    
+
     end
   end
 end
