2338ff1d8f9eb7a440a408aad95d63ea07334c55
Ben Atkins <benatkins@fullbridge.com>
2012-10-25 12:35:41 -0400

    Re-organizing the javascripts directory a bit, and adding an index file so the users can pull all of the javascripts into the asset pipeline of their rails app with a single call.

---
+++ vendor/assets/javascripts/s3_cors_fileupload/index.js
@@ -0,0 +1,6 @@
+//= require s3_cors_fileupload/vendor/jquery.ui.widget
+//= require s3_cors_fileupload/vendor/tmpl
+//= require s3_cors_fileupload/vendor/load-image
+//= require s3_cors_fileupload/jquery.iframe-transport
+//= require s3_cors_fileupload/jquery.fileupload
+//= require s3_cors_fileupload/jquery.fileupload-ui
