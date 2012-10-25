3e1252fed78e351d745a3ef2e0a70562d7b2fe05
Ben Atkins <benatkins@fullbridge.com>
2012-10-25 16:05:06 -0400

    Re-organizing the lib directory.

--- lib/s3_cors_fileupload.rb
+++ lib/s3_cors_fileupload.rb
@@ -1,2 +1,2 @@
-require 's3_cors_fileupload/rails'
 require 's3_cors_fileupload/version'
+require 's3_cors_fileupload/rails'
--- lib/s3_cors_fileupload/rails.rb
+++ lib/s3_cors_fileupload/rails.rb
@@ -1,7 +1,7 @@
 if defined?(::Rails)
   require 's3_cors_fileupload/rails/engine' if ::Rails.version >= '3.1'
-  require 's3_cors_fileupload/policy_helper'
-  require 's3_cors_fileupload/form_helper'
+  require 's3_cors_fileupload/rails/policy_helper'
+  require 's3_cors_fileupload/rails/form_helper'
 
   ActionView::Base.send(:include, S3CorsFileupload::FormHelper) if defined?(ActionView::Base)
 end
