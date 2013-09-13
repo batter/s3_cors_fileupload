f35f46734a7ef15f55a8034603ed8f71445ada50
Ben Atkins <benatkins@fullbridge.com>
2013-09-13 11:56:54 -0400

    Adding the Javascript-Load-Image meta data handler javascript which is a dependency for the jQuery-File-Upload

--- vendor/assets/javascripts/s3_cors_fileupload/index.js
+++ vendor/assets/javascripts/s3_cors_fileupload/index.js
@@ -1,6 +1,7 @@
 //= require s3_cors_fileupload/vendor/jquery.ui.widget
 //= require s3_cors_fileupload/vendor/tmpl
 //= require s3_cors_fileupload/vendor/load-image
+//= require s3_cors_fileupload/vendor/load-image-meta
 //= require s3_cors_fileupload/jquery.iframe-transport
 //= require s3_cors_fileupload/jquery.fileupload
 //= require s3_cors_fileupload/jquery.fileupload-process
