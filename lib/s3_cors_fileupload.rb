b84a13db0c9ac4d8c75eae5bf4eae6bc301fa805
Ben Atkins <batkinz@gmail.com>
2014-01-08 14:20:52 -0500

    Use ActiveRecord::VERSION::MAJOR for version comparisons isntead of invoking :to_f on the full version String

--- lib/s3_cors_fileupload.rb
+++ lib/s3_cors_fileupload.rb
@@ -3,6 +3,6 @@ require 's3_cors_fileupload/rails'
 
 module S3CorsFileupload
   def self.active_record_protected_attributes?
-    @active_record_protected_attributes ||= ActiveRecord::VERSION::STRING.to_f < 4.0 || !!defined?(ProtectedAttributes)
+    @active_record_protected_attributes ||= ::ActiveRecord::VERSION::MAJOR < 4 || !!defined?(ProtectedAttributes)
   end
 end
